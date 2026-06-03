2810eabb216a7481d38d01fd3f6a084242e35c3b
nirch <nirh2@yahoo.com>
2026-06-03 11:49:56 +0300

    Add math module with add, subtract functions and PI constant

---
+++ js-and-algo/modules/math-esmodules-consume.mjs
@@ -0,0 +1,5 @@
+
+// import math from "./math-esmodules.mjs";
+import {add, PI} from "./math-esmodules.mjs";
+
+console.log(add(5, PI));
---
+++ js-and-algo/modules/math-esmodules.mjs
@@ -0,0 +1,14 @@
+
+export function add(a, b) {
+  return a + b;
+}
+
+export function subtract(a, b) {
+  return a - b;
+}
+
+export const PI = 3.14;
+
+// export default 
+
+// export { add, subtract, PI}
