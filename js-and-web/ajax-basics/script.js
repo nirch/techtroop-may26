95753bd050c609618bd453e03296f0e004320df6
nirch <nirh2@yahoo.com>
2026-06-16 11:36:07 +0300

    Update AJAX functionality to use Axios for random dog image retrieval; enhance HTML heading

--- js-and-web/ajax-basics/script.js
+++ js-and-web/ajax-basics/script.js
@@ -3,15 +3,31 @@ addEventListener("DOMContentLoaded", () => {
 
   function randomImage() {
     fetch("https://dog.ceo/api/breeds/image/random")
-      .then(response => response.json())
-      .then(data => {
+      .then((response) => response.json())
+      .then((data) => {
         console.log(data);
         const randomImgURL = data.message;
         document.querySelector("#imgDog").src = randomImgURL;
-      })
+      });
+  }
+
+  function randomImageAxios() {
+    axios.get("https://dog.ceo/api/breeds/image/random").then((response) => {
+      console.log(response);
+      const randomImgURL = response.data.message;
+      document.querySelector("#imgDog").src = randomImgURL;
+    });
+
+    // fetch("https://dog.ceo/api/breeds/image/random")
+    //   .then((response) => response.json())
+    //   .then((data) => {
+    //     console.log(data);
+    //     const randomImgURL = data.message;
+    //     document.querySelector("#imgDog").src = randomImgURL;
+    //   });
   }
 
   document
     .querySelector("#btnRandomImg")
-    .addEventListener("click", randomImage);
+    .addEventListener("click", randomImageAxios);
 });
